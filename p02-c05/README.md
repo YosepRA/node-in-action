7a95c3a8ee1c78fb9e110b86eb21f683ac5ab8e5
YosepRA <yosep.ryan.yra@gmail.com>
2022-01-17 20:52:03 +0700

    Finish part 02 chapter 05.

---
+++ p02-c05/README.md
@@ -0,0 +1,7 @@
+## P02-S05: Storing Node Application Data
+
+Storing application's data using methods starting from memory and file system to DBMS (SQL and NoSQL).
+
+### Notes:
+
+Some of the examples are omitted because of the needs to install a specific databases. I have limited storage space at the time of learning this book, so I only read through it.
---
+++ p02-c05/src/file-storage.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+
+const args = process.argv.slice(2);
+const command = args.shift();
+const taskDescription = args.join(' ');
+const file = path.join(process.cwd(), '/data/.tasks');
+
+/* ========== Helpers ========== */
+
+function loadOrInitializeTaskArray(file, cb) {
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      cb([]);
+      throw err;
+    }
+
+    const tasks = JSON.parse(data || '[]');
+
+    cb(tasks);
+  });
+}
+
+function listTasks(file) {
+  loadOrInitializeTaskArray(file, (tasks) => {
+    tasks.forEach((task) => console.log(task));
+  });
+}
+
+function storeTasks(file, tasks) {
+  const data = JSON.stringify(tasks);
+
+  fs.writeFile(file, data, (err) => {
+    if (err) throw err;
+
+    console.log('Saved!');
+  });
+}
+
+function addTask(file, taskDescription) {
+  loadOrInitializeTaskArray(file, (tasks) => {
+    tasks.push(taskDescription);
+    storeTasks(file, tasks);
+  });
+}
+
+/* ========== Implementation ========== */
+
+switch (command) {
+  case 'list':
+    listTasks(file);
+    break;
+
+  case 'add':
+    addTask(file, taskDescription);
+    break;
+
+  default:
+    console.log(`Usage: node ${process.argv[1]} list|add [taskDescription]`);
+    break;
+}
